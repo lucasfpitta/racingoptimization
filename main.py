5f6da0351dc1ac12f3bfe41f46a3ca608e02c0d5
Danilo <danilo@MacBook-Air-de-Danilo.local>
2024-05-04 15:36:45 -0300

    Added the points reduction and smoothing

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-#All import files
 import numpy as np
 import pygame as gm
 import matplotlib.pyplot as plt
-import geopandas as gpd
 from shapely.geometry import Point, Polygon
 import pandas as pd
-import fiona
-fiona.drvsupport.supported_drivers['kml'] = 'rw'
-fiona.drvsupport.supported_drivers['KML'] = 'rw'
 import scipy as sci
+from Map_processing.mapreader import get_outline
+from Map_processing.point_reduction_and_smooting import reduce_points, smooth_moving_average
+
+alfa = 0.01
+n_neighborhood = 16
+max_slope = 0.06
+
+#read the map and set the metric coordinates
+right, left,  = get_outline('Map_processing/Maps_kml/catalunya_2022_right.kml','Map_processing/Maps_kml/catalunya_2022_left.kml')
+coords= reduce_points(right,left)
+#smooth array 2 and 5 (z coordinate)
+coords[2], coords[5] = smooth_moving_average(coords[0:3], coords[3:6],int(len(coords[0])/n_neighborhood),alfa,max_slope)
