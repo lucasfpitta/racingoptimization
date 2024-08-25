3e588e83285299e5b5f61b32bf9fc383f3114d14
Danilo <danilo@MacBook-Air-de-Danilo.local>
2024-08-25 18:17:11 -0300

    map of SEM

--- Map_processing/mapreader.py
+++ Map_processing/mapreader.py
@@ -7,40 +7,43 @@ fiona.drvsupport.supported_drivers['KML'] = 'rw'
 
 def get_outline(doc1, doc2):
 
-    my_map1 = gpd.read_file(doc1, driver='KML') #read kml of each side of the track
+    #gpd.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw'
+    my_map1 = gpd.read_file(doc1, driver='KML')
     my_map2 = gpd.read_file(doc2, driver='KML')
 
-    my_map1['points'] = my_map1.apply(lambda x: [y for y in x['geometry'].coords], axis=1) #extract the coordinates from the kml 
+
+    #my_map1['points'] = my_map1.apply(lambda l: linestring_to_points(l['geometry']), axis=1)
+    #my_map2['points'] = my_map2.apply(lambda l: linestring_to_points(l['geometry']), axis=1)
+
+    my_map1['points'] = my_map1.apply(lambda x: [y for y in x['geometry'].coords], axis=1)
     my_map2['points'] = my_map2.apply(lambda x: [y for y in x['geometry'].coords], axis=1)
 
-    coord_matrix1 = np.zeros((3,int(len(my_map1.points[0])/3+1)))
-    coord_matrix2 = np.zeros((3,int(len(my_map2.points[0])/3+1)))
+    coord_matrix1 = np.zeros((3,int(len(my_map1.points[0])+1)))
+    coord_matrix2 = np.zeros((3,int(len(my_map2.points[0])+1)))
 
-    #earth eliptical constants
-    a=6378137 #radii
+    a=6378137
     b=6356752.3142
-    e2=(a**2-b**2)/a**2 #excentricity
+    e2=(a**2-b**2)/a**2
     
-    #translate geografical coordinates into meters and set the origin as the first point
     for i in range(len(coord_matrix1[0])-1):
-        coord_matrix1[0,i]=(my_map1.points[0][3*i][0]-my_map1.points[0][0][0])*np.pi*a*np.cos(my_map1.points[0][0][0]*np.pi/180)/(180*np.sqrt(1-e2*np.sin(my_map1.points[0][0][0]*np.pi/180)))
-        coord_matrix1[1,i]=(my_map1.points[0][3*i+1][0]-my_map1.points[0][1][0])*np.pi*a*(1-e2)/(180*pow(1-e2*pow(np.sin(my_map1.points[0][1][0]*np.pi/180),2),3/2))
-        coord_matrix1[2,i]=my_map1.points[0][3*i+2][0]-my_map1.points[0][2][0]
+        coord_matrix1[0,i]=(my_map1.points[0][i][0]-my_map1.points[0][0][0])*np.pi*a*np.cos(my_map1.points[0][0][0]*np.pi/180)/(180*np.sqrt(1-e2*np.sin(my_map1.points[0][0][0]*np.pi/180)))
+        coord_matrix1[1,i]=(my_map1.points[0][i][1]-my_map1.points[0][0][1])*np.pi*a*(1-e2)/(180*pow(1-e2*pow(np.sin(my_map1.points[0][0][1]*np.pi/180),2),3/2))
+        coord_matrix1[2,i]=my_map1.points[0][i][2]-my_map1.points[0][0][2]
     coord_matrix1[0,-1]=coord_matrix1[0][0]
     coord_matrix1[1,-1]=coord_matrix1[1][0]
     coord_matrix1[2,-1]=coord_matrix1[2][0]
     for i in range(len(coord_matrix2[0])-1):
-        coord_matrix2[0,i]=(my_map2.points[0][3*i][0]-my_map1.points[0][0][0])*np.pi*a*np.cos(my_map1.points[0][0][0]*np.pi/180)/(180*np.sqrt(1-e2*np.sin(my_map1.points[0][0][0]*np.pi/180)))
-        coord_matrix2[1,i]=(my_map2.points[0][3*i+1][0]-my_map1.points[0][1][0])*np.pi*a*(1-e2)/(180*pow(1-e2*pow(np.sin(my_map1.points[0][1][0]*np.pi/180),2),3/2))
-        coord_matrix2[2,i]=my_map2.points[0][3*i+2][0]-my_map1.points[0][2][0]
-    
-    #connect the last point to the first
+        coord_matrix2[0,i]=(my_map2.points[0][i][0]-my_map1.points[0][0][0])*np.pi*a*np.cos(my_map1.points[0][0][0]*np.pi/180)/(180*np.sqrt(1-e2*np.sin(my_map1.points[0][0][0]*np.pi/180)))
+        coord_matrix2[1,i]=(my_map2.points[0][i][1]-my_map1.points[0][0][1])*np.pi*a*(1-e2)/(180*pow(1-e2*pow(np.sin(my_map1.points[0][0][1]*np.pi/180),2),3/2))
+        coord_matrix2[2,i]=my_map2.points[0][i][2]-my_map1.points[0][0][2]
     coord_matrix2[0,-1]=coord_matrix2[0][0]
     coord_matrix2[1,-1]=coord_matrix2[1][0]
     coord_matrix2[2,-1]=coord_matrix2[2][0]
     
+    
     return coord_matrix1, coord_matrix2
 
+
 if __name__ == "__main__":
-    right, left,  = get_outline('Map_processing/Maps_kml/catalunya_2022_right.kml','Map_processing/Maps_kml/catalunya_2022_left.kml')
+    right, left,  = get_outline('Map_processing/Maps_kml/extSEM.kml','Map_processing/Maps_kml/intSEM.kml')
     print(right,left)
--- main.py
+++ main.py
@@ -12,7 +12,14 @@ n_neighborhood = 16
 max_slope = 0.06
 
 #read the map and set the metric coordinates
-right, left,  = get_outline('Map_processing/Maps_kml/catalunya_2022_right.kml','Map_processing/Maps_kml/catalunya_2022_left.kml')
+right, left,  = get_outline('Map_processing/Maps_kml/extSEM.kml','Map_processing/Maps_kml/intSEM.kml')
 coords= reduce_points(right,left)
 #smooth array 2 and 5 (z coordinate)
 coords[2], coords[5] = smooth_moving_average(coords[0:3], coords[3:6],int(len(coords[0])/n_neighborhood),alfa,max_slope)
+plt.plot(right[0,:100], right[1,:100], 'gray',left[0,:100], left[1,:100], 'red')
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot3D(right[0], right[1], right[2], 'gray')
+ax.plot3D(left[0], left[1], left[2], 'red')
+plt.show()
+print("Hello World")
