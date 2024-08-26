3f9be0a3228340e52283c37e2f4dc4fd9e64b927
Danilo <danilo@MacBook-Air-de-Danilo.local>
2024-08-25 21:05:05 -0300

    added splines

--- main.py
+++ main.py
@@ -3,23 +3,38 @@ import pygame as gm
 import matplotlib.pyplot as plt
 from shapely.geometry import Point, Polygon
 import pandas as pd
-import scipy as sci
+import scipy as scp
+import random as rnd
 from Map_processing.mapreader import get_outline
 from Map_processing.point_reduction_and_smooting import reduce_points, smooth_moving_average
+from splines.splines import path_info
 
 alfa = 0.01
 n_neighborhood = 16
 max_slope = 0.06
+N=1000
 
 #read the map and set the metric coordinates
 right, left,  = get_outline('Map_processing/Maps_kml/extSEM.kml','Map_processing/Maps_kml/intSEM.kml')
 coords= reduce_points(right,left)
 #smooth array 2 and 5 (z coordinate)
 coords[2], coords[5] = smooth_moving_average(coords[0:3], coords[3:6],int(len(coords[0])/n_neighborhood),alfa,max_slope)
+
 plt.plot(right[0,:100], right[1,:100], 'gray',left[0,:100], left[1,:100], 'red')
+
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.plot3D(right[0], right[1], right[2], 'gray')
 ax.plot3D(left[0], left[1], left[2], 'red')
 plt.show()
-print("Hello World")
+
+alfas = np.random.random_sample(len(right[0]))
+alfas[-1]=alfas[0]
+spline, angle = path_info(left, right, alfas,N)
+
+fig = plt.figure()
+plt.plot(np.linspace(0,1,num = N),angle*360/2/3.14159) 
+plt.plot(np.linspace(0,1,num = N),np.sin(angle)*100)
+plt.show()
+
+
---
+++ splines/splines.py
@@ -0,0 +1,33 @@
+import numpy as np
+import scipy as scp
+
+def knot_points(left, right, alfas):
+    splpoints = np.zeros((2,len(right[0])))
+    for i in range(len(right[0])):
+        splpoints[0][i] = right[0][i]+alfas[i]*(left[0][i]-right[0][i])
+        splpoints[1][i] = right[1][i]+alfas[i]*(left[1][i]-right[1][i])
+    return splpoints
+
+def splines_and_derivatives(splpoints,N):
+    t = np.linspace(0,1,num = len(splpoints[0]))
+    interpol = scp.interpolate.CubicSpline(t, (splpoints[0],splpoints[1]),axis=1, bc_type='natural')
+    spline = interpol(np.linspace(0,1,num = N))
+    diff = interpol.derivative()
+    derivative = diff(np.linspace(0,1,num = N))
+    return spline, derivative
+
+def find_angle(derivative):
+    angle = np.zeros(len(derivative[0]))
+    for i in range(len(derivative[0])):
+        if derivative[0][i] >= 0:
+            angle[i] = np.arctan((derivative[1][i])/(derivative[0][i]))
+        else:
+            angle[i] = np.arctan((derivative[1][i])/(derivative[0][i]))+np.pi
+    angle[0]=angle[-1]
+    return angle
+
+def path_info(left, right, alfas,N):
+    splpoints = knot_points(left, right, alfas)
+    spline, derivative = splines_and_derivatives(splpoints,N)
+    angle = find_angle(derivative)
+    return spline, angle
